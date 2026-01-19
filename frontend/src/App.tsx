87037a1a05518557f74a58fe6b8dc2ce86187623
khianvictorycalderon <ckhianvictory@gmail.com>
2026-01-20 07:30:14 +0800

    Fixed paths

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,17 @@
+import { useState } from "react";
+import VerifyPage from "./pages/verify";
+
+type Pages = "verify";
+
 export default function App() {
+
+    const [page, setPage] = useState<Pages>("verify");
+
     return (
-      <div className="h-screen w-screen bg-neutral-900 text-yellow-400 font-semibold flex flex-col items-center justify-center">
-        <h2>MERN Spending Tracker</h2>
-        <p>Production Ready (Local) Project</p>
-      </div>
+      <>
+        {page === "verify" && (
+          <VerifyPage/>
+        )}
+      </>
     );
 }
---
+++ frontend/src/pages/verify.tsx
@@ -0,0 +1,7 @@
+export default function VerifyPage() {
+    return (
+        <div>
+            <h2>Verify Page</h2>
+        </div>
+    );
+}
