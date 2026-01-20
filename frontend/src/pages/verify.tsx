29508120c429707480b9081b330fe14f4dcff5bc
khianvictorycalderon <ckhianvictory@gmail.com>
2026-01-20 21:36:41 +0800

    Uninstalled react-hook-form: Prioritizes simplicity and rapid development

--- frontend/src/pages/verify.tsx
+++ frontend/src/pages/verify.tsx
@@ -3,8 +3,8 @@ import { HeadingText } from "../components/typography";
 export default function VerifyPage() {
     return (
         <div className="min-h-screen w-full flex flex-col items-center justify-center">
-            <div>
-                <HeadingText>Login</HeadingText>
+            <div className="px-8 py-4 bg-neutral-800 rounded-md shadow">
+                <HeadingText>Login Account</HeadingText>
             </div>
         </div>
     );
